72b273c21f1606f10f7e815fe0a09806f66b289f
Aladdin <aladdin90.se@gmail.com>
2024-10-31 11:21:41 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import LoginRegister from './views/sections/LoginRegister/LoginRegister.jsx';
 
 function App() {
   return (
     <div className='container'>
       <a className='btn-round-xl'><i class="fa-light fa-bars-staggered"></i></a>
+      <LoginRegister />
     </div>
   );
 }
