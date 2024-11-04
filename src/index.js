2ca7c031b9222621bcf56d765c257335938dfdc5
israel morales lara <130910243+lear23@users.noreply.github.com.>
2024-11-04 08:11:18 +0100

    Nu funkar psrtials

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Footer from './components/Footer';
-import Header from './components/Header';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Header />
+
     <App />
-    <Footer />
+   
   </React.StrictMode>
 );
 
