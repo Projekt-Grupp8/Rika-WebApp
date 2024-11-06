d7269a2e0f68d79b5c24c1345d330470bb129e8b
Douglas3478 <Douglas_00@live.se>
2024-11-06 12:38:14 +0100

    css color fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './app.min.css';
 import LoginRegister from './views/sections/LoginRegister/LoginRegister.jsx';
+
 
 function App() {
   return (
     <div className='container'>
       <a href='/' className='btn-round-xl'><i class="fa-light fa-bars-staggered"></i></a>
-      <LoginRegister />
     </div>
   );
 }
