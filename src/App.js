a601b0d0b9c8bb77d9ebf4a329e6362c3d646688
Douglas3478 <Douglas_00@live.se>
2024-11-06 12:40:09 +0100

    app.js loginregister

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className='container'>
       <a href='/' className='btn-round-xl'><i class="fa-light fa-bars-staggered"></i></a>
+      <LoginRegister />
     </div>
   );
 }
