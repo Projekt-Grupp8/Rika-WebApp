1abb7b7dfbdca49fac1264322f16d32b7daa40d1
Douglas3478 <Douglas_00@live.se>
2024-11-06 14:51:29 +0100

    verification view done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './app.min.css';
 import Login from './views/sections/Login/Login';
+import EmailVerify from './views/sections/EmailVerify/EmailVerify';
 
 function App() {
   return (
     <div className='container'>
-      <Login />
+      {/* <Login /> */}
+      <EmailVerify />
     </div>
   );
 }
